136765375cd0fde56f88d20c363313e4d19ce03d
fufukha <wafakhalid29@gmail.com>
2018-04-09 15:26:43 -0400

    add HTMLwebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
 		app: './src/index.js'
 	},
 	module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-        	loader: 'babel-loader',
-        	options: {
-        		presets: [
-        			'env', 
-        			'react'
-        		]
-        	}
-        }          
-      }
-    ]
-  },
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'env', 
+							'react'
+						]
+					}
+				}          
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Comments'
+		})
+	],
 	output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
 };
