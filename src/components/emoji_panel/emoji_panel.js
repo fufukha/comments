bbf696f20d7f97056d4009f37cb3e02389f13c6e
fufukha <wafakhalid29@gmail.com>
2018-05-07 18:29:28 -0400

    removed FontAwesomeIcon components add emoji characters with span

--- src/components/emoji_panel/emoji_panel.js
+++ src/components/emoji_panel/emoji_panel.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 export default class EmojiPanel extends Component {
 	render () {
+		const emojis = ["😃", "😀", "🙄"];
+
+		const emojiSpans = emojis.map( emoji => 
+			<span onClick={this._handleClick.bind(this)}>{emoji}</span>
+		);
+
 		return (
 			<div className='emoji-panel'>
 				<div className='wrapper-emoji'>
-					<FontAwesomeIcon icon="smile"/>
-					<FontAwesomeIcon icon="smile"/>
-					<FontAwesomeIcon icon="smile"/>
-					<FontAwesomeIcon icon="smile"/>
+					{emojiSpans}
 				</div>
 			</div>
 		)
+	}
+
+	_handleClick (event) {
+		const emoji = event.target.innerHTML; 
+		this.props.appendEmoji(emoji);
 	}
 }
