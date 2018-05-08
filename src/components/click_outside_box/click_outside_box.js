5588975d1e084a7d24bf3e89379fa3b607e3c8e2
fufukha <wafakhalid29@gmail.com>
2018-05-08 15:40:00 -0400

    first commit

---
+++ src/components/click_outside_box/click_outside_box.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class ClickOutsideBox extends React.Component {
+  componentDidMount() {
+    document.addEventListener("click", () => {
+			//if outside return empty string-falsey
+			const isInside = (event.target).closest(this.props.selector);
+			if ( !isInside ) {
+				this.props.hideElement();
+			}
+		});
+  }
+  
+  render(){
+    return (
+      <div>{this.props.children}</div>
+    );  
+  }  
+}
