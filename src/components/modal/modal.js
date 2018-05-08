d451a0286e63c13be1fc01c2aac1b8b1fae65c18
fufukha <wafakhalid29@gmail.com>
2018-05-08 18:21:57 -0400

    undo clickOutsideBox implementation

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import ClickOutsideBox from '../click_outside_box/click_outside_box';
 
 export default class Modal extends Component {
 	render() {
 		const { hideModal, deleteReply } = this.props;
 		return (
-			<div className='modal_bg'>
-				<ClickOutsideBox
-					selector=".modal_content"
-					hideElement={hideModal}
-				>
-					<div className='modal_content'>
-						<p className='modal_text'>Are you sure you want to delete your reply?</p>
-						<div className='wrapper_btn'>
-							<button onClick={hideModal}>Cancel</button>
-							<button onClick={deleteReply}>Delete</button>
-						</div>
+			<div className='modal_bg'>				
+				<div className='modal_content'>
+					<p className='modal_text'>Are you sure you want to delete your reply?</p>
+					<div className='wrapper_btn'>
+						<button onClick={hideModal}>Cancel</button>
+						<button onClick={deleteReply}>Delete</button>
 					</div>
-				</ClickOutsideBox>
+				</div>
 			</div>      
 		)
 	}
