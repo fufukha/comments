0f175e9672e2d3d13549b4609aeb2b678595b8c2
fufukha <wafakhalid29@gmail.com>
2018-05-10 12:43:23 -0400

    pass hideDisplay to this.props.children

--- src/components/toggle_box/toggle_box.js
+++ src/components/toggle_box/toggle_box.js
@@ -10,12 +10,14 @@ export default class ToggleBox extends Component {
 		 };
 
 		 this._toggleDisplay = this._toggleDisplay.bind(this);
+		 this._hideDisplay = this._hideDisplay.bind(this)
 	}
 
 	componentDidMount() {
 		document.addEventListener("click", () => {
 			//if outside return empty string-falsey
 			const isInside = (event.target).closest(".toggleBox");
+	
 			if ( !isInside ) {
 				this._hideDisplay();
 			}
@@ -24,20 +26,23 @@ export default class ToggleBox extends Component {
 
 	render() {
 		const { isDisplay } = this.state;
-		const { className, title, children, toggleBtn } = this.props;
+		const { classNames, title, children, toggleBtn, _hideDisplay } = this.props;
+    const childrenWithProps = React.Children.map(children, child =>
+      React.cloneElement(child, { hideDisplay: this._hideDisplay}));
+
 
 		return (
 			<div className="toggleBox">
 				<div
-					className={className}
+					className={classNames}
 					title={title}
-
+					role="button"
 					onClick={this._toggleDisplay}>
 						<FontAwesomeIcon icon={toggleBtn}/>
 				</div>
 				{isDisplay &&
 					<div>
-						{children}
+						{childrenWithProps}
 					</div>}
 			</div>
 		);
