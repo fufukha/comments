5a3b558beb1f85fb37f538f25c4006664c9c6c09
fufukha <wafakhalid29@gmail.com>
2018-05-10 12:40:33 -0400

    change so props from ToggleBox controls hiding the Modal when canceled.

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 
 export default class Modal extends Component {
 	render() {
-		const { hideModal, deleteReply } = this.props;
-		return (
-			<div className='modal_bg'>				
-				<div className='modal_content'>
-					<p className='modal_text'>Are you sure you want to delete your reply?</p>
-					<div className='wrapper_btn'>
-						<button onClick={hideModal}>Cancel</button>
-						<button onClick={deleteReply}>Delete</button>
-					</div>
+		const { hideDisplay, deleteReply } = this.props;
+		return (		
+			<div className='modal_content'>
+				<p className='modal_text'>Are you sure you want to delete your reply?</p>
+				<div className='wrapper_btn'>
+					<button onClick={hideDisplay}>Cancel</button>
+					<button onClick={deleteReply}>Delete</button>
 				</div>
-			</div>      
+			</div>			   
 		)
 	}
 }
