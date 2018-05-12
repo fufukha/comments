7941444f1130dd1d90d6f503c0c3acdeb8c18b7a
fufukha <wafakhalid29@gmail.com>
2018-05-12 10:47:31 -0400

    add TabIndexLoopBox and componentDidMount to autofocus and class names cancel/delete

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import TabIndexLoopBox from '../tabindex_loop_box/tabindex_loop_box'
 
 export default class Modal extends Component {
+	componentDidMount(){
+		document.getElementById("first-tab_modal").focus();
+	}
+
 	render() {
 		const { hideDisplay, deleteReply } = this.props;
+		const id1 = "first-tab_modal";
+		const id2 = "last-tab_modal";
+
 		return (		
 			<div className='modal_content'>
 				<p className='modal_text'>Are you sure you want to delete your reply?</p>
-				<div className='wrapper_btn'>
-					<button onClick={hideDisplay}>Cancel</button>
-					<button onClick={deleteReply}>Delete</button>
-				</div>
+				<TabIndexLoopBox firstTabId={id1} lastTabId={id2} className="wrapper_btn">
+					<button id={id1} className="cancel" onClick={hideDisplay}>Cancel</button>
+					<button id={id2} className="delete" onClick={deleteReply}>Delete</button>
+				</TabIndexLoopBox>
 			</div>			   
 		)
 	}
